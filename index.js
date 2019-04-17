c8f58678fd04e0d731a7c1d1d65612b5e168a423
Maxsteelcooldan <46035576+Maxsteelcooldan@users.noreply.github.com>
2019-04-17 16:22:22 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+var player, controller;
 
+player {
+  x = 0,
+  y = 0,
+  x_velocity = 0,
+  y_velocity = 0,
+  jumping:false,
+  height = 10,
+  width = 10
+  };
+controller {
+  down:false,
+  up:false,
+  left:false,
+  right:false,
+  keyListener:function(event) {
+    
+    var yeet=
+  }
+};
