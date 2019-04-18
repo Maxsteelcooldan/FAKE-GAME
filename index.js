ba391bb48caf03a1997d2e6fb5c6f3e0eb07deca
Maxsteelcooldan <46035576+Maxsteelcooldan@users.noreply.github.com>
2019-04-18 18:26:11 -0400

    Update index.js

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ context.canvas.width = 100;
 context.canvas.height = 100;
 
 player = {
-  x = 0;
-  y = 0;
-  x_velocity = 0;
-  y_velocity = 0;
-  jumping:false;
-  height = 10;
-  width = 10;
+  x: 0,
+  y: 0,
+  x_velocity: 0,
+  y_velocity: 0,
+  jumping:false,
+  height:10,
+  width: 10
   };
 controller = {
   down:false,
